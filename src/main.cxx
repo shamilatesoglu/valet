676a5859be4226571c27b12bf968d56f729dc683
M. Samil Atesoglu <m.samilatesoglu@gmail.com>
2022-10-26 22:50:27 +0300

    Basic logging

--- src/main.cxx
+++ src/main.cxx
@@ -7,13 +7,15 @@
 #include <unordered_set>
 #include <vector>
 
-#include "../third_party/toml.hpp"
+#include "../third_party/spdlog/spdlog.h"
+#include "../third_party/tomlplusplus/toml.hpp"
 
 struct package_t {
   std::string name;
   std::string version;
   std::string std;
   std::filesystem::path compiler_path;
+  std::vector<std::filesystem::path> include_paths;
 };
 
 std::optional<std::filesystem::path>
@@ -33,15 +35,15 @@ void collect_source_files(std::filesystem::path const &folder,
   for (auto const &entry : std::filesystem::directory_iterator(folder)) {
     if (entry.is_directory()) {
       if (auto nested_autob = find_autob(entry.path())) {
-        std::cout << "Found another package: " << *nested_autob << std::endl;
+        spdlog::info("Found another package: {}",
+                     nested_autob->generic_string());
         // TODO: Figure out how to do nested packages.
         continue;
       }
       collect_source_files(entry.path(), out);
     } else if (source_file_extensions.contains(entry.path().extension())) {
       out.push_back(entry.path());
-      std::cout << "Found source file: " << entry.path().generic_string()
-                << std::endl;
+      spdlog::info("Found source file: {}", entry.path().generic_string());
     }
   }
 }
@@ -51,13 +53,20 @@ compile(package_t const &package, std::filesystem::path const &output_folder,
         std::filesystem::path const &source_file) {
   std::filesystem::path obj_file =
       output_folder / (source_file.filename().string() + ".o");
-  std::string cmd = package.compiler_path.generic_string() + " -c " +
-                    source_file.generic_string() + " -std=" + package.std +
-                    " -o " + obj_file.generic_string();
-  int ret = std::system(cmd.c_str());
+  std::stringstream cmd;
+  cmd << package.compiler_path.generic_string() << " -c "
+      << source_file.generic_string() << " -std=" << package.std << " -o "
+      << obj_file.generic_string();
+  for (auto const &incl : package.include_paths) {
+    cmd << " -I" << incl.generic_string();
+  }
+  auto cmdstr = cmd.str();
+  spdlog::debug("Compile command: {}", cmdstr);
+  spdlog::info("Compiling: {}", source_file.generic_string());
+  int ret = std::system(cmdstr.c_str());
   if (ret != 0) {
-    std::cout << "Failed to compile " << source_file << ". Return code: " << ret
-              << std::endl;
+    spdlog::error("Failed to compile {}. Return code: {}",
+                  source_file.generic_string(), ret);
     return std::nullopt;
   }
   return obj_file;
@@ -66,46 +75,61 @@ compile(package_t const &package, std::filesystem::path const &output_folder,
 bool link(package_t const &package,
           std::vector<std::filesystem::path> const &obj_files,
           std::filesystem::path const &output_folder) {
+  auto output_file_path = output_folder / package.name;
   std::stringstream cmd;
   cmd << package.compiler_path.generic_string();
   for (auto const &obj : obj_files) {
     cmd << " " << obj;
   }
-  cmd << " -o " << (output_folder / package.name);
-  int ret = std::system(cmd.str().c_str());
+  cmd << " -o " << output_file_path;
+  auto cmdstr = cmd.str();
+  spdlog::debug("Link command: {}", cmdstr);
+  spdlog::info("Linking: {}", output_file_path.generic_string());
+  int ret = std::system(cmdstr.c_str());
   if (ret != 0) {
-    std::cout << "Failed to link. Return code: " << ret << std::endl;
+    spdlog::error("Failed to link. Return code: {}", ret);
     return false;
   }
   return true;
 }
 
-int main(int argc, char *argv[]) {
-  std::cout << "[autob]" << std::endl;
+package_t parse_autob(std::filesystem::path const &cfg_file_path) {
+  auto cfg_tbl = toml::parse_file(cfg_file_path.generic_string());
+  auto const &package_toml = cfg_tbl["package"];
+  package_t package{
+      package_toml["name"].value_or<std::string>(""),
+      package_toml["version"].value_or<std::string>(""),
+      package_toml["std"].value_or<std::string>(""),
+      package_toml["compiler_path"].value_or<std::string>(""),
+  };
+  auto const &include_paths_ref = package_toml["include_paths"];
+  if (auto path_arr = include_paths_ref.as_array()) {
+    for (auto &&path_node : *path_arr) {
+      package.include_paths.push_back(*path_node.value<std::string>());
+    }
+  }
+  return package;
+}
 
+int main(int argc, char *argv[]) {
+  spdlog::set_level(spdlog::level::debug);
+  spdlog::set_pattern("%^[%=8l] %v%$");
   try {
     std::filesystem::path work_folder(argv[1]);
     std::filesystem::path output_folder = work_folder / "build";
     std::filesystem::path source_folder = work_folder / "src";
     std::filesystem::path test_folder = work_folder / "test";
 
-    auto cfg_file = find_autob(work_folder);
-    if (!cfg_file) {
-      std::cout << "Cannot find autob config file under directory "
-                << work_folder << std::endl;
+    auto cfg_file_path = find_autob(work_folder);
+    if (!cfg_file_path) {
+      spdlog::error("Cannot find autob config file under directory {}",
+                    work_folder.generic_string());
       return 1;
     }
-    std::cout << "Using autob configuration " << cfg_file->generic_string()
-              << std::endl;
+    spdlog::info("Using autob configuration {}",
+                 cfg_file_path->generic_string());
 
-    auto cfg_tbl = toml::parse_file(cfg_file->generic_string());
-    auto const &package_toml = cfg_tbl["package"];
-    package_t package{
-        package_toml["name"].value_or<std::string>(""),
-        package_toml["version"].value_or<std::string>(""),
-        package_toml["std"].value_or<std::string>(""),
-        package_toml["compiler_path"].value_or<std::string>(""),
-    };
+    package_t package = parse_autob(*cfg_file_path);
 
     std::filesystem::create_directory(output_folder);
 
@@ -113,20 +137,19 @@ int main(int argc, char *argv[]) {
     collect_source_files(source_folder, source_files);
     std::vector<std::filesystem::path> obj_files;
     for (auto const &src : source_files) {
-      std::cout << "Compiling: " << src.generic_string() << std::endl;
       if (auto obj_file = compile(package, output_folder, src)) {
         obj_files.push_back(*obj_file);
       }
     }
 
     if (link(package, obj_files, output_folder)) {
-      std::cout << "Success." << std::endl;
+      spdlog::info("Success.");
       return 0;
     } else
       return -1;
 
   } catch (const toml::parse_error &err) {
-    std::cerr << "Parsing failed:" << std::endl << err << std::endl;
+    spdlog::error("Parsing failed: {}", err.description());
     return 1;
   }
 
