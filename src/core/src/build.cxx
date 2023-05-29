c76e9ab7a8f73eac176b0f704961cdbba47d6f51
M. Samil Atesoglu <m.samilatesoglu@gmail.com>
2023-05-29 23:46:44 +0300

    Format

--- src/core/src/build.cxx
+++ src/core/src/build.cxx
@@ -245,9 +245,9 @@ std::optional<BuildPlan> BuildPlan::make(DependencyGraph<Package> const& package
 		return std::nullopt;
 	auto const& sorted = *sorted_opt;
 	BuildPlan plan;
-    auto rec = std::max(1u, platform::get_cpu_count() / 2) - 1;
-    auto nth = std::max(1u, rec);
-    spdlog::debug("Using {} threads to build", nth);
+	auto rec = std::max(1u, platform::get_cpu_count() / 2) - 1;
+	auto nth = std::max(1u, rec);
+	spdlog::debug("Using {} threads to build", nth);
 	plan.thread_pool = std::make_shared<util::ThreadPool>(nth);
 	plan.package_graph = package_graph;
 	for (auto const& package : sorted) {
